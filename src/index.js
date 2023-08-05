f5560ddba78e2d7bd65201ecef7deb2d4d34ea20
Diana Smilian <dianka.kulakevich@gmail.com>
2023-08-06 01:30:53 +0300

    finish

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import hello from './js/hello';
+import './sass/main.scss';
+import ukraine from './assets/uk.png';
+
+const mainImage = document.getElementById('mainImage');
+mainImage.src = ukraine;
